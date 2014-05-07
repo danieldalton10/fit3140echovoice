a9fd4b31e2dd5bd4f8edd6626f6993059855e196
Daniel Dalton <daniel.dalton10@gmail.com>
2014-05-07 20:26:18 +1000

    don't add compile files just source code

--- bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/daniel/VersionControl/git/fit3140echovoice/bin/AndroidManifest.xml : \
-/home/daniel/VersionControl/git/fit3140echovoice/AndroidManifest.xml \
--- bin/VTamper-debug-unaligned.apk.d
+++
@@ -1,3 +0,0 @@
-/home/daniel/VersionControl/git/fit3140echovoice/bin/VTamper-debug-unaligned.apk : \
-/home/daniel/VersionControl/git/fit3140echovoice/bin/VTamper.ap_ \
-/home/daniel/VersionControl/git/fit3140echovoice/bin/classes.dex \
