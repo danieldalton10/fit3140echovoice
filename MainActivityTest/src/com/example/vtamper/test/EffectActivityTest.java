466068e81a3dad0f736340e2e61300a3ec4f5064
cyyeap <cyyeap.pg@gmail.com>
2014-05-16 10:11:42 +1000

    effectactivitytest doesnt work the button

---
+++ MainActivityTest/src/com/example/vtamper/test/EffectActivityTest.java
@@ -0,0 +1,55 @@
+package com.example.vtamper.test;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.example.vtamper.EffectActivity;
+
+import android.test.AndroidTestCase;
+import android.view.View;
+import junit.framework.TestCase;
+
+public class EffectActivityTest extends AndroidTestCase {
+
+	
+	EffectActivity a = new EffectActivity();
+	public void testOnDestroy() {
+	//	fail("Not yet implemented");
+	}
+
+	public void testOnCreateBundle() {
+	//	fail("Not yet implemented");
+	}
+
+	public void testLoadFile() {
+	//	fail("Not yet implemented");
+	}
+
+	public void testOnReverse() {
+	//	fail("Not yet implemented");
+	}
+
+	public void testOnEcho() {
+		//fail("Not yet implemented");
+	}
+
+	public void testGetFile()throws IOException {
+		
+     	//assertNotNull(a.getFile("vtamper_clip_test.wav"));
+	}
+
+	public void testOnSave() {
+	//   assertNotNull(a.View);
+	}
+	
+
+	public void testOnPlay() {
+		View view;
+		a.onPlay(view);
+	}
+
+	public void testOnShare() {
+		//fail("Not yet implemented");
+	}
+
+}
